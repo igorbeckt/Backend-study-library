31c0c75a985d85dc147d282b1ca94bc06371bd76
Igor Nascimento <igorbeckt@hotmail.com>
2023-04-09 20:15:03 -0300

    Aula21
    Loop Do While

---
+++ C#/Aulas/Aula21(Loop DO WHILE - Estruturas de iteração)/Aula21.1.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Aula20{
+    static void Main(){
+
+        string senha="123";
+        string senhauser;
+        int tentativas=0;
+
+
+        do{
+            Console.Clear();
+            Console.WriteLine("Digite a senha");
+            senhauser=Console.ReadLine();    
+            tentativas++;
+        }while(senha != senhauser);
+
+        Console.WriteLine("Senha incorreta, tentativas:{0},tentativas");         
+    } 
+}
---
+++ C#/Aulas/Aula21(Loop DO WHILE - Estruturas de iteração)/Aula21.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Aula20{
+    static void Main(){
+
+        int num=5;
+        do{
+            Console.WriteLine("CFB Cursos");
+        }while(num<5);         
+    } 
+}
