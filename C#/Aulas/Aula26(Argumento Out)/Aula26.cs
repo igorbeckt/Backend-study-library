ae5718d7a45ab5592e097d09502e3b7833a27d53
Igor Nascimento <igorbeckt@hotmail.com>
2023-04-10 18:54:21 -0300

    Aula26
    Argumento Out

---
+++ C#/Aulas/Aula26(Argumento Out)/Aula26.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Aula24{
+    
+    static void Main(){
+        int divid,divis,quoc,rest;
+        divid=10;
+        divis=3;
+        quoc=divide(divid,divis,out rest);
+
+        Console.WriteLine("{0}/{1}:quociente={2} e resto={3}",divid,divis,quoc,rest);
+
+    }
+
+    static int divide(int dividendo, int divisor,out int resto){
+        int quociente;
+        quociente=dividendo/divisor;
+        resto=dividendo%divisor;
+        return quociente;
+    }
+
+}
