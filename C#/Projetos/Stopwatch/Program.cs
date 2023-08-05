6d456544808bf6e19018f7f22fd6b41f4f8c4b78
Igor Nascimento <97426648+igorbeckt@users.noreply.github.com>
2023-08-05 20:15:15 -0300

    Added project stopwatch
    Added project stopwatch

---
+++ C#/Projetos/Stopwatch/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu(){            
+            Console.Clear();
+            Console.WriteLine("S = segundos => 10s = 10 segundos");
+            Console.WriteLine("M = minutos => 1m = 1 minuto");
+            Console.WriteLine("0s = sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if(type == 'm')
+             multiplier = 60;
+
+            if(time == '0')
+             System.Environment.Exit(0);
+
+            PreStart(time * multiplier);                        
+        }
+
+        static void PreStart(int time){
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("GO...!");
+            Thread.Sleep(1500);
+
+            Start(time);
+        }
+        static void Start(int time){
+
+            int currentTime = 0;
+
+            while(currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("finished stopwatch");
+            Thread.Sleep(2500);
+            Menu();
+        }
+    }
+}
+
+
