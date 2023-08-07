9f49d349e50149399d2fe2cd76392bc82aea4958
Igor Nascimento <97426648+igorbeckt@users.noreply.github.com>
2023-08-07 00:56:34 -0300

    adding sln vs in project Stopwatch
    adding sln vs in project Stopwatch

---
+++ C#/Projetos/Stopwatch/Operation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    public static class Operation
+    {
+        public static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("GO...!");
+            Thread.Sleep(1500);
+
+            Start(time);
+        }
+        public static void Start(int time)
+        {
+
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("finished stopwatch");
+            Thread.Sleep(2500);
+            Menu.Show();
+        }
+    }
+}
