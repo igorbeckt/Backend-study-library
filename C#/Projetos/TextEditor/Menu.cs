0f41ec0cafbe228a48ea3a6029605df45370ac5a
Igor Nascimento <97426648+igorbeckt@users.noreply.github.com>
2023-08-07 01:02:10 -0300

    adding sln vs in project TextEditor
    adding sln vs in project TextEditor

---
+++ C#/Projetos/TextEditor/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEditor
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("1 - abrir arquivo");
+            Console.WriteLine("2 - criar novo arquivo");
+            Console.WriteLine("0 - sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Operation.Abrir(); break;
+                case 2: Operation.Editar(); break;
+                default: Menu.Show(); break;
+            }
+        }
+    }
+}
---
+++ C#/Projetos/TextEditor/Operation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEditor
+{
+    public static class Operation
+    {
+        public static void Abrir()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho do arquivo?");
+            string path = Console.ReadLine();
+
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu.Show();
+        }
+
+        public static void Editar()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (Pressione ESC para sair)");
+            Console.WriteLine("-------------------------");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Operation.Salvar(text);
+        }
+
+        public static void Salvar(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho para salvar o arquivo?");
+            var path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+
+            Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+            Console.ReadLine();
+            Menu.Show();
+        }
+    }
+}
