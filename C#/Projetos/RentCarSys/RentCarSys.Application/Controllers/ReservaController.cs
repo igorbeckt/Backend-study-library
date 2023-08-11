f7ec64d0032b713fef815f596cfb4d095b4f3d41
Igor Nascimento <97426648+igorbeckt@users.noreply.github.com>
2023-08-11 16:18:26 -0300

    finalizing the implementation of the service Cliente OK Veiculo OK Reserva OK

--- C#/Projetos/RentCarSys/RentCarSys.Application/Controllers/ReservaController.cs
+++ C#/Projetos/RentCarSys/RentCarSys.Application/Controllers/ReservaController.cs
@@ -4,6 +4,7 @@ using Localdorateste.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RentCarSys.Application.Interfaces;
+using RentCarSys.Application.Services;
 using RentCarSys.Enums;
 
 namespace RentCarSys.Application.Controllers
@@ -12,200 +13,96 @@ namespace RentCarSys.Application.Controllers
     [Route("/v1/reservas")]
     public class ReservaController : ControllerBase
     {
-        private readonly IReservasRepository _reservasRepository;
-        private readonly IClientesRepository _clientesRepository;
-        private readonly IVeiculosRepository _veiculosRepository;
+        private readonly ClienteService _clienteService;
+        private readonly VeiculoService _veiculoService;
+        private readonly ReservaService _reservaService;
 
-        public ReservaController(IReservasRepository reservasRepository, IClientesRepository clientesRepository, IVeiculosRepository veiculosRepository)
+        public ReservaController(ClienteService clienteService,
+                                 VeiculoService veiculo,
+                                 ReservaService reservaService)
         {
-            _reservasRepository = reservasRepository;
-            _clientesRepository = clientesRepository;
-            _veiculosRepository = veiculosRepository;
+            _clienteService = clienteService;
+            _veiculoService = veiculo;
+            _reservaService = reservaService;
         }
 
-        [HttpGet("buscarTodos")]
-        public async Task<IActionResult> BuscarReservas()
+        [HttpGet("buscarTodas")]
+        public async Task<IActionResult> BuscarReserva()
         {
-            try
+            var result = await _reservaService.BuscarTodasReservas();
+            if (result.Erros.Count > 0)
             {
-                var reservas = await _reservasRepository.ObterTodasReservasAsync();
-                return Ok(new ResultViewModel<List<Reserva>>(reservas));
-            }// Buscando todas as reservas no Banco 
-            catch
-            {
-                return StatusCode(500, value: new ResultViewModel<List<Reserva>>(erro: "05X05 - Falha interna no servidor!"));
-            }// Tratando erro
+                return StatusCode(500, result);
+            }
+
+            return Ok(result);
         }
 
-
-        [HttpGet("buscarPorId/{reservaid}")]
-        public async Task<IActionResult> BuscarReservaPorId(
+        [HttpGet("buscarPorId/{reservaid:int}")]
+        public async Task<IActionResult> BuscarReservasId(
         [FromRoute] int reservaid)
         {
-            try
+            var result = await _reservaService.BuscarReservaPorId(reservaid);
+            if (result.Erros.Count > 0)
             {
-                var reserva = await _reservasRepository.ObterReservaPorIdAsync(reservaid);
-                if (reserva == null)
-                    return NotFound(new ResultViewModel<Reserva>(erro: "A reserva não foi encontrada, verifique se a reserva já foi cadastrada!"));
-                // Validação de ID
-
-                return Ok(new ResultViewModel<Reserva>(reserva));
-                // Buscando a reserva no banco
+                return StatusCode(500, result);
             }
-            catch
-            {
-                return StatusCode(500, value: new ResultViewModel<Reserva>(erro: "Falha interna no servidor!"));
-            }// Tratando erro
 
-        }
-
+            return Ok(result);
+        }        
 
         [HttpPost("cadastrar")]
-        public async Task<IActionResult> CriarReserva(
+        public async Task<IActionResult> CriarReservas(
         [FromBody] EditorReservaViewModel model)
-
-
         {
             if (!ModelState.IsValid)
-                return BadRequest(error: new ResultViewModel<Reserva>(ModelState.PegarErros()));
-            // Configuração de padronização de erro
-
-            try
             {
-                var cliente = await _clientesRepository.ObterClientePorIdAsync(model.ClienteId);
-                if (cliente == null)
-                    return NotFound(new ResultViewModel<Reserva>(erro: "Cliente não encontrado, insira um cliente cadastrado!"));
-                // Validação do cliente
-
-                var veiculo = await _veiculosRepository.ObterVeiculoPorIdAsync(model.VeiculoId);
-                if (veiculo == null)
-                    return NotFound(new ResultViewModel<Reserva>(erro: "Veiculo não encontrado, insira um veiculo cadastrado!"));
-                // Validação do veiculo
-
-                if (cliente.Status == ClienteStatus.Running)
-                    return NotFound(new ResultViewModel<Reserva>(erro: "Não foi possivel alterar o cliente,possui reserva em andamento"));
-                // Validação para saber se o cliente não tem uma reserva em andamento
-
-                if (veiculo.Status == VeiculoStatus.Running)
-                    return NotFound(new ResultViewModel<Reserva>(erro: "Não foi possivel alterar o veiculo, possui reserva em andamento"));
-                // Validação para saber se o veiculo já foi alugado
-
-
-                var reserva = new Reserva
-
-                {
-                    Status = ReservaStatus.Online,
-                    DataReserva = model.DataReserva,
-                    ValorLocacao = model.ValorLocacao,
-                    DataRetirada = model.DataRetirada,
-                    DataEntrega = model.DataRetirada,
-                    Cliente = new List<Cliente> { cliente },
-                    Veiculo = new List<Veiculo> { veiculo }
-                };
-
-                await _reservasRepository.AdicionarReservaAsync(reserva);
-
-                return Created(uri: $"v1/reservas/{reserva.Id}", new ResultViewModel<Reserva>(reserva));
-                // Criação da reserva               
+                return BadRequest(new ResultViewModel<Reserva>(ModelState.PegarErros()));
             }
-            catch (DbUpdateException ex)
+
+            var result = await _reservaService.CriarReserva(model);
+
+            if (result.Erros.Count > 0)
             {
-                return StatusCode(500, new ResultViewModel<Reserva>(erro: "05XE8 - Não foi possível criar a reserva!"));
-            }// Tratando erro
-            catch
-            {
-                return StatusCode(500, new ResultViewModel<Reserva>(erro: "05X10 - Falha interna no servidor!"));
-            }// Tratando Erro
+                return StatusCode(500, result);
+            }
+
+            return Created($"reservas/{result.Dados.Id}", result);
         }
 
-
-        [HttpPut("alterar/{reservaid}")]
-        public async Task<IActionResult> EditarReservas(
+        [HttpPut("alterar/{reservaid:int}")]
+        public async Task<IActionResult> EditarClientes(
         [FromRoute] int reservaid,
         [FromBody] EditorReservaViewModel model)
         {
             if (!ModelState.IsValid)
-                return BadRequest(error: new ResultViewModel<Reserva>(ModelState.PegarErros()));
-            // Configuração de padronização de erro
-
-            try
             {
-                var reserva = await _reservasRepository.ObterReservaPorIdAsync(reservaid);
-                if (reserva == null)
-                    return NotFound(new ResultViewModel<Reserva>(erro: "Reserva não encontrada!"));
-                // Validação da reserva
-
-                /*if (reserva.Status == ReservaStatus.Running)
-                    return NotFound(new ResultViewModel<Reserva>(erro: "Não foi possivel alterar a reserva, possui um contrato em aberto!"));
-                // Validação para saber se a reserva está contrato em aberto*/
-
-                if (reserva.Status == ReservaStatus.Running)
-                    return NotFound(new ResultViewModel<Reserva>(erro: "Não foi possivel alterar a reserva, o veiculo já foi retirado e a reserva está em andamento!"));
-                // Validação para saber se a reserva está andamento
-
-                if (reserva.Status == ReservaStatus.Offline)
-                    return NotFound(new ResultViewModel<Reserva>(erro: "Não é possivel alterar uma reserva finalizada!"));
-                // Validação para saber se a reserva foi finalizada*/
-
-                reserva.DataRetirada = model.DataRetirada;
-                reserva.DataEntrega = model.DataEntrega;
-                reserva.ValorLocacao = model.ValorLocacao;
-
-                await _reservasRepository.AtualizarReservaAsync(reserva);
-
-
-                return Ok(new ResultViewModel<Reserva>(reserva));
-                // Alteração da Reserva
+                return BadRequest(new ResultViewModel<Reserva>(ModelState.PegarErros()));
             }
-            catch (DbUpdateException ex)
-            {
-                return StatusCode(500, new ResultViewModel<Reserva>(erro: "05XE8 - Não foi possível alterar a reserva!"));
-            }// Tratando erros
-            catch (Exception ex)
-            {
-                return StatusCode(500, new ResultViewModel<Reserva>(erro: "05X11 - Falha interna no servidor!"));
-            }// Tratando erros
 
+
+            var result = await _reservaService.EditarReserva(reservaid, model);
+
+            if (result.Erros.Count > 0)
+            {
+                return StatusCode(500, result);
+            }
+
+            return Ok(result);
         }
 
-
-
-        [HttpDelete("excluir/{reservaid}")]
-        public async Task<IActionResult> ExcluirReservas(
+        [HttpDelete("excluir/{reservaid:int}")]
+        public async Task<IActionResult> ExcluirClientes(
         [FromRoute] int reservaid)
         {
-            try
+            var result = await _reservaService.ExcluirReserva(reservaid);
+
+            if (result.Erros.Count > 0)
             {
-                var reserva = await _reservasRepository.ObterReservaPorIdAsync(reservaid);
-                if (reserva == null)
-                    return NotFound(new ResultViewModel<Reserva>(erro: "Reserva não encontrada, verifique se reserva foi cadastrada!"));
-                // Validação da reserva
-
-                /*if (reserva.StatusReserva == "Pago")
-                    return NotFound(new ResultViewModel<Reserva>(erro: "Não foi possivel alterar a reserva, possui um contrato em aberto!"));
-                // Validação para saber se a reserva está contrato em aberto*/
-
-                if (reserva.Status == ReservaStatus.Running)
-                    return NotFound(new ResultViewModel<Reserva>(erro: "Não foi possivel alterar a reserva, o veiculo já foi retirado e a reserva está em andamento!"));
-                // Validação para saber se a reserva está andamento
-
-                if (reserva.Status == ReservaStatus.Offline)
-                    return NotFound(new ResultViewModel<Reserva>(erro: "Não é possivel alterar uma reserva finalizada!"));
-                // Validação para saber se a reserva foi finalizada
-
-                await _reservasRepository.ExcluirReservaAsync(reserva);
-
-                return Ok(new ResultViewModel<Reserva>(reserva));
-                // Remoção da reserva
+                return StatusCode(500, result);
             }
-            catch (DbUpdateException ex)
-            {
-                return StatusCode(500, new ResultViewModel<Reserva>(erro: "05XE7 - Não foi possível excluir a reserva!"));
-            }// Tratando erro
-            catch (Exception ex)
-            {
-                return StatusCode(500, new ResultViewModel<Reserva>(erro: "05X12 - Falha interna no servidor!"));
-            }// Tratando erro
+
+            return Ok(result);
         }
     }
 }
